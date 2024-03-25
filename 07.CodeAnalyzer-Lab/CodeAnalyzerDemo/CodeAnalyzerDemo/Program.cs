93337d84079b7a5fd73ae84fbecfa6b798b22fba
Simeon <simeonsavov21@gmail.com>
2024-03-25 16:26:23 +0200

    feat: Add folder with CodeAnalyzer Demo for C#

---
+++ 07.CodeAnalyzer-Lab/CodeAnalyzerDemo/CodeAnalyzerDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// This is the Main class of the Program.
+/// </summary>
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hi!");
+        Console.WriteLine(MakeGreeting("Simeon"));
+    }
+
+    private static string MakeGreeting(string name)
+    {
+        if (name == null) // No braces and spacing issues
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        return "Hello, " + name;
+    }
+}
